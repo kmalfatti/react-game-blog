735bce70d8ccda1ab39989cb9354f9fe200f843b
Kyle Malfatti <kyle.malfatti@gmail.com>
2017-07-09 18:07:55 -0700

    created blogpost component

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import '../styles/App.css';
 import Navbar from './Navbar'
 import Carousel from './Carousel'
+import Blogpost from './Blogpost'
 
 export default class App extends Component {
   render() {
     return (
-      
         <div className="App">
-        <Navbar />
-        <Carousel />
+          <Navbar />
+          <Carousel />
+          <Blogpost />
         </div>
-      
     );
   }
 }
---
+++ src/components/Blogpost.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Blogpost extends Component {
+  render() {
+    return (
+      <div className='blogpost'>
+        <img src="http://via.placeholder.com/750x450" alt=""/>
+        <h3>This is a blogpost title</h3>
+        <p>This is a lot more information about the blogpost or like a describption also works</p>
+        <p><i>July, 9, 2017</i></p>
+      </div>
+    );
+  }
+}
