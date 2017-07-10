3d48a4aea38002d768c3675e81111cdc514bf203
Kyle Malfatti <kyle.malfatti@gmail.com>
2017-07-09 17:38:04 -0700

    created carousel placeholder

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 import Navbar from './Navbar'
+import Carousel from './Carousel'
 
 export default class App extends Component {
   render() {
@@ -8,7 +9,7 @@ export default class App extends Component {
       
         <div className="App">
         <Navbar />
-       
+        <Carousel />
         </div>
       
     );
