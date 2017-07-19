5059f1978f67f0ab74f9928fdb3978a21dc3567d
Kyle Malfatti <kyle.malfatti@gmail.com>
2017-07-19 16:34:36 -0700

    update blogpost styling to bootstrap

--- src/components/Blogpost.jsx
+++ src/components/Blogpost.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Blogpost extends Component {
   render() {
     return (
-      <div className='blogpost'>
+      <div className='blogpost col-md-4'>
         <img src="http://via.placeholder.com/750x450" alt=""/>
         <h3>This is a blogpost title</h3>
         <p>This is a lot more information about the blogpost or like a describption also works</p>
