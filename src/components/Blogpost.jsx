237192ba583e35fc2f774f9b771af978002e197f
Kyle Malfatti <kyle.malfatti@gmail.com>
2017-07-20 14:24:44 -0700

    update blogpost text to display over image

--- src/components/Blogpost.jsx
+++ src/components/Blogpost.jsx
@@ -5,9 +5,11 @@ export default class Blogpost extends Component {
     return (
       <div className='blogpost col-md-4'>
         <img src="http://via.placeholder.com/750x450" alt=""/>
-        <h3>This is a blogpost title</h3>
-        <p>This is a lot more information about the blogpost or like a describption also works</p>
-        <p><i>July, 9, 2017</i></p>
+        <div className='blogpost-desc'>
+          <h3><a href="">This is a blogpost title</a></h3>
+          <p>This is a lot more information about the blogpost or like a describption also works</p>
+          <p><i>July 9, 2017</i></p>
+        </div>
       </div>
     );
   }
