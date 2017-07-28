c91843ddb6fcd68a805b2a5c08ffd72a930cea5e
Kyle Malfatti <kyle.malfatti@gmail.com>
2017-07-28 13:28:17 -0700

    update feedpost style

--- src/components/Feedpost.jsx
+++ src/components/Feedpost.jsx
@@ -3,15 +3,17 @@ import React, { Component } from 'react';
 export default class Feedpost extends Component {
   render(){
     return(
-      <article className='feedpost row'>
-        <img src="http://via.placeholder.com/750x450" alt="" className='feedImg col-md-4'/>
-        <div className='feedDesc col-md-8'>
-          <h3>This is a title</h3>
-          <p>July 11, 2017 | Kyle Malfatti</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet expedita laborum inventore blanditiis voluptate, cupiditate, doloremque tempora similique odio perferendis quasi, amet et est perspiciatis sequi quod vitae adipisci ad.<a href="#">Read More</a></p>
-        </div>
+      <div>
+        <article className='feedpost row'>
+          <img src="http://via.placeholder.com/750x450" alt="" className='feedImg col-md-4'/>
+          <div className='feedDesc col-md-8'>
+            <h3>This is a title</h3>
+            <p>July 11, 2017 | Kyle Malfatti</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet expedita laborum inventore blanditiis voluptate, cupiditate, doloremque tempora similique odio perferendis quasi, amet et est perspiciatis sequi quod vitae adipisci ad.<a href="#">Read More</a></p>
+          </div>
+        </article>
         <hr/>
-      </article>
+      </div>
   )}
 }
 
