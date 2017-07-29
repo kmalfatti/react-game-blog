3ac4b1912fba35425252bc366d288e15fe8ae0ab
Kyle Malfatti <kyle.malfatti@gmail.com>
2017-07-29 14:26:36 -0700

    update feedpost links and hover opacity

--- src/components/Feedpost.jsx
+++ src/components/Feedpost.jsx
@@ -5,9 +5,9 @@ export default class Feedpost extends Component {
     return(
       <div>
         <article className='feedpost row'>
-          <img src="http://via.placeholder.com/750x450" alt="" className='feedImg col-md-4'/>
+          <a href=''><img src="http://via.placeholder.com/750x450" alt="" className='feedImg col-md-4'/></a>
           <div className='feedDesc col-md-8'>
-            <h3>This is a title</h3>
+            <a href=''><h3>This is a title</h3></a>
             <p>July 11, 2017 | Kyle Malfatti</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet expedita laborum inventore blanditiis voluptate, cupiditate, doloremque tempora similique odio perferendis quasi, amet et est perspiciatis sequi quod vitae adipisci ad.<a href="#">Read More</a></p>
           </div>
